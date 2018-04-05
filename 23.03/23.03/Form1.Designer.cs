245552b9868d28a5b732b2d40db02f98999d0675
MarinaKuvshinova <pn-05@ukr.net>
2018-04-05 12:38:26 +0300

    new file

---
+++ 23.03/23.03/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace _23._03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(292, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(340, 107);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(284, 150);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(283, 78);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 277);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ 23.03/23.03/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace _23._03
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source = CR4-12\SQLEXPRESS; Initial Catalog = BookShop; Integrated Security = true;";
+        SqlConnection sqlConnection = null;
+        DataTable dataTable = null;
+        public Form1()
+        {
+            InitializeComponent();
+            sqlConnection = new SqlConnection(connectionString);
+            btnStart.Click += btnStart_Click;
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView2.DataSource = null;
+            dataGridView1.DataSource = await GetDataFirstAsync();
+            dataGridView2.DataSource = await GetDataSecondAsync();
+        }
+
+        private async Task<object> GetDataSecondAsync()
+        {
+            using (sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+                string sql = "select * from Authors;";
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+                dataTable = new DataTable();
+                SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+                int line = 0;
+                do
+                {
+                    while (await sqlDataReader.ReadAsync())
+                    {
+                        if (line++ == 0)
+                        {
+                            for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                            {
+                                dataTable.Columns.Add(sqlDataReader.GetName(i));
+                            }
+                        }
+                        DataRow dataRow = dataTable.NewRow();
+                        for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                        {
+                            dataRow[i] = await sqlDataReader.GetFieldValueAsync<Object>(i);
+                        }
+                        dataTable.Rows.Add(dataRow);
+                    }
+                } while (await sqlDataReader.NextResultAsync());
+                return dataTable;
+            }
+        }
+
+        private async Task<object> GetDataFirstAsync()
+        {
+            using (sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+                string sql = "waitfor delay '00:00:10';";
+                sql += "select * from Books;";
+                SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+                dataTable = new DataTable();
+                SqlDataReader sqlDataReader = await sqlCommand.ExecuteReaderAsync();
+                int line = 0;
+                do
+                {
+                    while (await sqlDataReader.ReadAsync())
+                    {
+                        if (line++ == 0)
+                        {
+                            for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                            {
+                                dataTable.Columns.Add(sqlDataReader.GetName(i));
+                            }
+                        }
+                        DataRow dataRow = dataTable.NewRow();
+                        for (int i = 0; i < sqlDataReader.FieldCount; i++)
+                        {
+                            dataRow[i] = await sqlDataReader.GetFieldValueAsync<Object>(i);
+                        }
+                        dataTable.Rows.Add(dataRow);
+                    }
+                } while (await sqlDataReader.NextResultAsync());
+                return dataTable;
+            }
+        }
+    }
+}
